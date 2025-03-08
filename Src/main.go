3d91c2ae207dcbb949ab068da6107b8e739e3d04
mistereskere <you@example.com>
2025-03-08 21:09:20 +0100

    Add torrent session and magnet link handling in main.go

--- Src/main.go
+++ Src/main.go
@@ -4,7 +4,9 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
+	"github.com/cenkalti/rain/torrent"
 	"github.com/joho/godotenv"
 	"github.com/webtor-io/go-jackett"
 )
@@ -18,6 +20,20 @@ func main() {
 	}
 	apiURL := os.Getenv("JACKETT_API_URL")
 	apiKey := os.Getenv("JACKETT_API_KEY")
+
+	magnetLink := "magnet:?xt=urn:btih:740ECC60CE537C342B67B4895C22B02077E77832&dn=Klobgniak+%28v0.3.0.0%29+%5BFitGirl+Repack%5D&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.theoks.net%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.ccp.ovh%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=https%3A%2F%2Ftracker.tamersunion.org%3A443%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=http%3A%2F%2Ftracker.bt4g.com%3A2095%2Fannounce&tr=udp%3A%2F%2Fbt2.archive.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fbt1.archive.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.filemail.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker1.bt.moack.co.kr%3A80%2Fannounce&tr=http%3A%2F%2Fopen.acgnxtracker.com%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce"
+
+	// Create a session
+	ses, _ := torrent.NewSession(torrent.DefaultConfig)
+
+	// Add magnet link
+	tor, _ := ses.AddURI(magnetLink, nil)
+
+	// Watch the progress
+	for range time.Tick(time.Second) {
+		s := tor.Stats()
+		log.Printf("Status: %s, Downloaded: %d, Peers: %d", s.Status.String(), s.Bytes.Completed, s.Peers.Total)
+	}
 
 	// Check if running inside a container
 	if os.Getenv("DOCKER") == "true" {
@@ -39,7 +55,7 @@ func main() {
 
 	// Start the servers
 	log.Print("Starting server on :8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(":8081", nil); err != nil {
 		log.Fatal(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,38 @@ go 1.23.6
 
 require (
 	github.com/joho/godotenv v1.5.1
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/webtor-io/go-jackett v0.0.0-20201110160721-0d56a2f41070
-	golang.org/x/net v0.0.0-20201110031124-69a78807bb2b // indirect
+)
+
+require (
+	github.com/cenkalti/backoff/v3 v3.2.2 // indirect
+	github.com/cenkalti/log v1.0.0 // indirect
+	github.com/cenkalti/rain v1.13.0 // indirect
+	github.com/gofrs/uuid v4.4.0+incompatible // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/google/btree v1.1.3 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/jackpal/bencode-go v1.0.0 // indirect
+	github.com/juju/ratelimit v1.0.2 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/minio/sha256-simd v1.0.0 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mr-tron/base58 v1.2.0 // indirect
+	github.com/multiformats/go-multihash v0.2.3 // indirect
+	github.com/multiformats/go-varint v0.0.6 // indirect
+	github.com/nictuku/dht v0.0.0-20201226073453-fd1c1dd3d66a // indirect
+	github.com/nictuku/nettools v0.0.0-20150117095333-8867a2107ad3 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/powerman/rpc-codec v1.2.2 // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
+	github.com/spaolacci/murmur3 v1.1.0 // indirect
+	github.com/youtube/vitess v3.0.0-rc.3+incompatible // indirect
+	github.com/zeebo/bencode v1.0.0 // indirect
+	go.etcd.io/bbolt v1.3.11 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	lukechampine.com/blake3 v1.1.6 // indirect
 )
